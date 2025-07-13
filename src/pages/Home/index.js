9e3e4cb546f3fda2dad80f9738477117e5d88489
Paulo Magio <paullomagio@gmail.com>
2025-07-13 16:21:26 -0300

    Inicializa projeto PrimeFlix

---
+++ src/pages/Home/index.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import api from "../../services/api"; // Caminho corrigido (ajuste conforme necessário)
+import { Link } from "react-router-dom";
+import './home.css';
+
+function Home() {
+  const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get("movie/now_playing", {
+        params: {
+          api_key: "f6a0625e0f122c367af3a9294f79ed6d",
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+      setFilmes(response.data.results.slice(0, 10))
+      setLoading(false);
+    }
+
+    loadFilmes();
+  }, []);
+
+  if(loading){
+    return(
+        <div className = 'loading'>
+            <h2>Carrgando filmes...</h2>
+        </div>
+    )
+  }
+
+  return (
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
